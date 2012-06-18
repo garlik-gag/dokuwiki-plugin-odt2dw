bd93cb766bb83a0561171c7cf0ebbeada009748f
Mark <mc.prins@gmail.com>
2012-06-18 11:00:58 +0200

    add nl translation

---
+++ odt2dw/lang/nl/settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Dutch language setting file
+ *
+ * @author Mark Prins <mprins@users.sf.net>
+ * @author Greg BELLAMY <garlik.crx@gmail.com> [Gag]
+ */
+
+$lang['debugLvl']                 = "Debug modus -- meer verbose : 0-void, 1-toon fout, 2-Log&toon fouten, 3-Log&toon alle berichten";
+$lang['logFile']                  = "Pad naar het log bestand";
+$lang['formDisplayRule']          = "TODO Actie lijst waar het importeer bestand formulier wordt getoond";
+$lang['formIntroMessage']         = "<div>TODO Te tonen bericht voor het importeer bestand formulier</div><div>  - DokuWiki syntax toegstaan</div><div>  - verstek waarde : verstek waarde -> de tekst uit de gelokaliseerde instellingen</div>";
+$lang['formMaxFileSize']          = "<div>TODO maximum bestandsgrootte</div><div>De waarde moet lager zijn dan de Apache max size waarde</div>";
+$lang['parserPostDisplay']        = "TODO Getoond masker na een import";
+$lang['parserXslFile']            = "Naam van het xsl bestand dat gebruikt wordt voor de translatie";
+$lang['parserLinkToOriginalFile'] = "Maak een link naar het originele bestand in de DokuWiki pagina";
+$lang['parserCoreTimeOut']        = "Speciale TimeOut gebruikt tijdens ontleden van de odt inhoud met xslt (verstek waarde : 300).";
+$lang['parserUploadDir']          = "Pad waar het bestand wordt opgeslagen";
